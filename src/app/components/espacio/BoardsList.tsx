83d9268473ed98799a5773e86fb4bef85b4293c9
Vania Velazquez <velazquezvania000@gmail.com>
2023-11-20 20:42:08 -0300

    Agregado tarjetas en parte superior del tablero

--- src/app/components/espacio/BoardsList.tsx
+++ src/app/components/espacio/BoardsList.tsx
@@ -10,28 +10,35 @@ interface IBoarList {
 export const BoardList = () => {
   const [active, setActive] = useState(0);
 
-  const store = useContext(SpaceContext)
-  const { boards, setBoardActive, userActive } = useStore(store, (s) => s)
+  const store = useContext(SpaceContext);
+  const { boards, setBoardActive, userActive } = useStore(store, (s) => s);
 
-  const handleActive = async(i: number) => {
+  const handleActive = async (i: number) => {
     setActive(() => i);
     const boardActive = boards.find((tablero, index) => index == i) as IBoard;
-    const {data} = await getBoard(boardActive.id, userActive.id)
-    setBoardActive({ nombre: data.nombre, id: data.id, columnas: data.columnas });
+    const { data } = await getBoard(boardActive.id, userActive.id);
+    setBoardActive({
+      nombre: data.nombre,
+      id: data.id,
+      columnas: data.columnas,
+    });
   };
 
   return (
-    <>
+    <div className="w-full h-full flex grid-flow-col gap-3 my-3 justify-start">
       {boards?.map((item: any, index) => (
         <div
           role="button"
-          className={`boardItem ${index == active ? "active" : ""}`}
+          className={`w-52 h-20 p-4 bg-cyan-400 hover:bg-sky-400 border rounded-md boardItem ${
+            index == active ? "active" : ""
+          }`}
           onClick={() => handleActive(index)}
           key={item.id}
         >
           {item.nombre}
         </div>
       ))}
-    </>
+    </div>
   );
+  
 };
